257476b6138275513d08e25eba2585d9d61a379e
serhiizx <1990743+serhiizx@users.noreply.github.com>
2026-05-30 14:37:01 +0000

    Deploying to gh-pages from @ serhiizx/goit-advancedjs-hw-01@961ec8bcb7b6a546e9131fba67cf05034782e7a4 🚀

---
+++ 1-gallery-BV0OYn8S.js
@@ -0,0 +1,2 @@
+import"./js/styles-B4OpFi0w.js";import{S as a}from"./js/vendor-Dd0k82OA.js";new a(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});
+//# sourceMappingURL=1-gallery-BV0OYn8S.js.map
