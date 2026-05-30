4ea03bc4caaff1e122a54c7300a6e5aaa50c8cdf
serhiizx <1990743+serhiizx@users.noreply.github.com>
2026-05-30 14:09:11 +0000

    Deploying to gh-pages from @ serhiizx/goit-advancedjs-hw-01@8b5662571db5dc7e1801451c8f6a9df7b0149d71 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-DmnAI2Rp.js";import{S as a}from"./assets/vendor-CgTBfC_f.js";new a(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});
+import"./js/styles-B4OpFi0w.js";import{S as a}from"./js/vendor-Dd0k82OA.js";new a(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});
 //# sourceMappingURL=1-gallery.js.map
